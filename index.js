b95a6d6899dae5751605d4f5a88ccb4b8364880d
Faust Fu <faust.fu@gmail.com>
2018-01-09 11:35:33 +0800

    Dev: inject meta to index template file.

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const Vue = require('vue')
 const fs = require('fs')
 const server = require('express')()
 const renderer = require('vue-server-renderer').createRenderer({
-  template: fs.readFileSync('./index.template.html', 'utf-8')
+  template: fs.readFileSync('./index.template.html', 'utf-8'),
 })
+
+const context = {
+  title: 'UU is good',
+  meta: `<meta charset="UTF-8">`,
+}
 
 server.get('*', (req, res) => {
   const app = new Vue({
     data: {
-      url: req.url
+      url: req.url,
     },
-    template: '<div>訪問的 UU 是：{{ url }}</div>'
+    template: `<div>访问的 URL 是： {{ url }}</div>`,
   })
-  renderer.renderToString(app, (err, html) => {
+  renderer.renderToString(app, context, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
